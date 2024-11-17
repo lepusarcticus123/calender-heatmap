3019ce55d4b6009a2833a0fc579142ea3da39a43
lepus <1035047456@qq.com>
2024-11-17 15:28:11 +0800

    基本架构完成1.0.0

---
+++ vite.config.js
@@ -0,0 +1,38 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: "index.js", // 入口文件
+      name: "Heatmap", // 库的全局变量名
+      fileName: "heatmap", // 输出文件的文件名前缀
+    },
+    rollupOptions: {
+      external: [], // 配置外部依赖项，例如lodash
+      output: [
+        {
+          format: "umd", // UMD 格式
+          dir: "dist", // 输出目录
+          entryFileNames: "heatmap.umd.js", // UMD 输出文件名
+          name: "Heatmap", // 全局变量名
+          globals: {
+            // 如果有外部依赖，可以在这里配置
+            // lodash: '_',
+          },
+        },
+        {
+          format: "es", // ESM 格式
+          dir: "dist", // 输出目录
+          entryFileNames: "heatmap.esm.js", // ESM 输出文件名
+        },
+        {
+          format: "cjs", // CommonJS 格式
+          dir: "dist", // 输出目录
+          entryFileNames: "heatmap.cjs.js", // CommonJS 输出文件名
+        },
+      ],
+    },
+    minify: true, // 启用压缩
+    sourcemap: true, // 生成 sourcemap 文件
+  },
+});
